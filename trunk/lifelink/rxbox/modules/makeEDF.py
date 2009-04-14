02c2747618f79765a25a72b72833c6af0653be83
arlanroie.santos <arlanroie.santos@34d7ff0e-e4a4-11dd-a6ea-a32d6b26a33d>
2009-04-14 01:17:58 +0000

    added sending of edf file to the triage

--- trunk/lifelink/rxbox/modules/makeEDF.py
+++ trunk/lifelink/rxbox/modules/makeEDF.py
@@ -1,54 +1,74 @@
-import wx
+"""
+Project LifeLink: makeEDF module
+
+A threading class which is called every after 15 seconds
+to convert all gathered telemetry from the biomedical modules
+to an EDF file. This EDF file will be stored locally and then
+transmitted to the Central side for diagnosis.
+
+Authors: Julius Miguel J. Broma
+         Arlan Roie A. Santos
+         Luis G. Sison, PhD
+         ------------------------------------------------
+         Instrumentation, Robotics and Control Laboratory
+         University of the Philippines - Diliman
+         ------------------------------------------------
+         March 2009
+"""
+
+from edf import EDF, Patient
+
+import datetime
 import threading
-import edf
-import time
-
-import socket
+import TriageClient
 
 
-class makeEDFThr(threading.Thread):
+class SaveThread (threading.Thread):
+    """create EDF file from the telemetry, store locally then transmit to the Central side via the Web Services"""
 
-    def __init__(self, ParentWindow, StartTime):
+    def __init__(self, parent):
 
         threading.Thread.__init__(self)
-        self.stopmakeEDFThr = threading.Event()
-        self.stopmakeEDFThr.clear()
+        self.stopSaveThread = threading.Event()
+        self.stopSaveThread.clear()
 
-        self.ParentWindow = ParentWindow
-#        self.BioSignals = BioSignals
-        self.StartTime = StartTime
+        self.parentPanel = parent
+        self.Endtime = datetime.datetime.today()
+        self.Starttime = self.Endtime + datetime.timedelta(seconds = -15)
 
+        # set formatting of time values
+        self.strStarttime = self.Starttime.strftime("%H.%M.%S")
+        self.strDate = self.Starttime.strftime("%d.%m.%y")
+        self.strY2KDate = self.Starttime.strftime("%d-%b-%Y")
+        self.timestamp = self.Starttime.strftime("%H%M%S")
 
     def stop(self):
-        self.stopmakeEDFThr.set()
+        """stop making and transmitting EDF files"""
+        
+        self.stopSaveThread.set()
 
     def run(self):
+        """create the EDF file"""
 
-        while True:
+        myPatient = Patient('12345', 'Julius Miguel' , 'Juarez', 'Broma', 'Rosal', 'Male', '02.17.86', 22)
+        myEDF     = EDF(myPatient, self.parentPanel.BioSignals, self.strDate, self.strStarttime, self.strY2KDate + \
+                        ': LifeLink 15 second data of CorScience modules', \
+                        '15', '1')
+        edffile   = open('EDF Files/' + 'PatientX' + '_' + self.timestamp + '.edf', 'wb+')
+        edfbin    = myEDF.get()
+        edffile.write(edfbin)
+        edffile.close()
 
-            if self.stopmakeEDFThr.set():
-                print 'break'
-                break
-#            print self.ParentWindow.BioSignals
-#            print'Check if BioSignal is equal to 3'
-#            if (len(self.ParentWindow.BioSignals) == 3):
-            if (len(self.ParentWindow.BioSignals) == 2):
-                print ' creating EDF file '
-                myPatient = edf.Patient('39309', 'Julius Miguel', 'Juarez', 'Broma', 'Rosal', 'Male', '03.17.86', 22)
+        print "created edf file"
 
-                myEDF = edf.EDF(myPatient, self.ParentWindow.BioSignals, time.strftime("%d.%m.%y"), self.StartTime, time.strftime("%d-%b-%Y") + \
-                            '15 second data of CorScience Pulse Oximeter module', \
-                            '15', '1')
-        #        edffile = open('C:/Users/ARLAN ROIE SANTOS/Desktop/SPO2EDF' + str(self.ParentWindow.count) + '.edf', 'wb+')
-                edffile = open('ECG_SPO2_BPM' + str(self.ParentWindow.count) + '.edf', 'wb+')
-                edffile.write(myEDF.get())
-                edffile.close()
-                self.ParentWindow.count += 1
+        # clear BioSignals
+        self.parentPanel.BioSignals = []
 
-                self.StartTime = time.strftime("%H.%M.%S")
-                self.ParentWindow.BioSignals = []
-
-                print len(self.ParentWindow.BioSignals)
-#                print 'Calling ContinueDAQ'
-#                wx.CallAfter(self.ParentWindow.ContinueDAQ)
-       
+        # check if the Refer button is disabled, meaning there is an active connection
+        # sends the created EDF file to the Triage only if the Refer Button has been clicked
+        if not self.parentPanel.Refer_Button.IsEnabled():
+            TriageClient.Triage().sendEDF('032509', edfbin)
+            print "EDF file sent\n"
+   
+        self.stop()
+        
