c2c925d052f97aadaaa3bd5c611d79c7516aac5c
rgfernandez <rgfernandez@34d7ff0e-e4a4-11dd-a6ea-a32d6b26a33d>
2009-06-05 03:37:11 +0000

    1) tabs converted to 4 spaces 2) for no existing sequence_id/case_id, target_path is removed 3)

--- trunk/eccs/MsgHandler/trunk/web/upload_file.php
+++ trunk/eccs/MsgHandler/trunk/web/upload_file.php
@@ -1,8 +1,8 @@
 <?php
 
 if (empty($_POST['case_id'])) {
-	echo -3;
-	die();
+    echo -3;
+    die();
 }
 
 
@@ -10,20 +10,24 @@ $target_path = "uploads/" .$_POST['case_id']. "/";
 $FUP = $_FILES["file"];
 
 if (empty($_POST['sequence_id'])){
-    $target_path = $IMAGEPATH;
+    $target_path = "";
 }
 
 
-if ($FUP["error"] > 0) {
+if ($FUP['error'] > 0) {
     echo -1;
 }
 else {
-    if (!is_dir($target_path)){ echo $target_path; mkdir($target_path); } 
-    $target_path = $target_path . basename( $FUP['name']); 
+    if (!is_dir($target_path)) {
+        echo $target_path;
+        mkdir($target_path);
+    } 
 
+    $target_path = $target_path . basename( $FUP['name']);
+    
     // Do overwrite check before this
     if(move_uploaded_file($FUP['tmp_name'], $target_path)) {
-	echo 0;
+        echo 0;
     } else{
         echo -2;
     }
