0344c2600a0785a07193c9aa7598a04dc9f712a2
rgfernandez <rgfernandez@34d7ff0e-e4a4-11dd-a6ea-a32d6b26a33d>
2009-08-07 08:52:31 +0000

    modified parameters to accept upon class init call

--- eccs/MsgHandler/trunk/main/python/dbutil.py
+++ eccs/MsgHandler/trunk/main/python/dbutil.py
@@ -9,14 +9,14 @@ class DbWrapper:
     """Wrapper for MySQL db"""
     def __init__(self, *args, **kwds):
         self.dbparams = dict()
-        if 'sql_host' in kwds:
-            self.dbparams['host'] = kwds['sql_host']
-        if 'sql_port' in kwds:
-            self.dbparams['port'] = int(kwds['sql_port'])
+        if 'host' in kwds:
+            self.dbparams['host'] = kwds['host']
+        if 'port' in kwds:
+            self.dbparams['port'] = int(kwds['port'])
         
-        self.dbparams['user'] = kwds['sql_user']
-        self.dbparams['passwd'] = kwds['sql_pwd']
-        self.dbparams['db'] = kwds['sql_db']
+        self.dbparams['user'] = kwds['user']
+        self.dbparams['passwd'] = kwds['passwd']
+        self.dbparams['db'] = kwds['db']
         
         log.debug('Using %s' % (str(self.dbparams)))
         #self.dbparams = {'host' : host, 'port' : port, 'user' : user, 'passwd' : passwd, 'db' : db}
@@ -28,6 +28,8 @@ class DbWrapper:
     
     def get(self, table, cols, conds, misc=''):
         """Helper for query."""
+        # misc is for LIMIT, GROUP BY, ORDER BY etc
+        
         if not(isinstance(cols, list) and cols):
             cols = ['*']
         if conds:
