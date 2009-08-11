7bfcab8203169322fc5a8398b227e22f6459b561
rgfernandez <rgfernandez@34d7ff0e-e4a4-11dd-a6ea-a32d6b26a33d>
2009-08-11 10:08:57 +0000

    ECCS-MsgHandler: reporting is back to 24-hour rule

--- eccs/MsgHandler/branches/chits/dev/report.py
+++ eccs/MsgHandler/branches/chits/dev/report.py
@@ -63,7 +63,7 @@ class report:
     def get_pre_apt(self, health_center=''):
         """Return contact and body of message from uuid."""
         cur = self.conn.cursor()
-        conds = {'timestampdiff(day, appointment_time, now())': '0'}
+        conds = {'timestampdiff(hour, appointment_time, now()) BETWEEN 0 AND 24': ''}
         if health_center:
             conds['health_center'] = health_center
         qry = self.get('patient_apts JOIN patient_regs ON patient_regs.id=patient_reg_id', ['health_center', 'count(health_center)'], conds, 'GROUP BY health_center')
@@ -75,8 +75,8 @@ class report:
     def get_post_apt(self, health_center=''):
         """Return contact and body of message from uuid."""
         cur = self.conn.cursor()
-        conds = {'timestampdiff(day, appointment_time, now())': '0',
-                 '(patient_reg_id NOT IN (SELECT patient_reg_id FROM patient_apts WHERE timestampdiff(day, appointment_time, now()) < 0))': ''}
+        conds = {'timestampdiff(hour, appointment_time, now()) BETWEEN 0 AND 24': '',
+                 '(patient_reg_id NOT IN (SELECT patient_reg_id FROM patient_apts WHERE timestampdiff(hour, appointment_time, now()) < 0))': ''}
         if health_center:
             conds['health_center'] = health_center
         qry = self.get('patient_apts JOIN patient_regs ON patient_regs.id=patient_reg_id', ['health_center', 'count(health_center)'], conds, 'GROUP BY health_center')
@@ -85,10 +85,21 @@ class report:
         x = cur.fetchall()
         return tuple([elem for elem in filter(None, map((lambda x: x if x[0] else ''), x))])
     
+    def db_get_defaults(self, health_center=''):
+        cur = self.conn.cursor()
+        conds = {'timestampdiff(hour, appointment_time, now()) BETWEEN 0 AND 24': '',
+                 '(patient_reg_id NOT IN (SELECT patient_reg_id FROM patient_apts WHERE timestampdiff(hour, appointment_time, now()) < 0))': ''}
+        if health_center:
+            conds['health_center'] = health_center
+        qry = self.get('patient_apts JOIN patient_regs ON patient_regs.id=patient_reg_id', ['patient_reg_id', ], conds)
+        cur.execute(*qry)
+        x = cur.fetchall()
+        return tuple(['T%04d' % elem[0] for elem in x])
+    
     def get_post_reg(self, health_center=''):
         """Return contact and body of message from uuid."""
         cur = self.conn.cursor()
-        conds = {'timestampdiff(day, create_time, now())': '0'}
+        conds = {'timestampdiff(hour, create_time, now()) BETWEEN 0 AND 24': ''}
         if health_center:
             conds['health_center'] = health_center
         qry = self.get('patient_regs', ['health_center', 'count(health_center)'], conds, 'GROUP BY health_center')
@@ -105,12 +116,18 @@ class report:
             y = self.get_post_apt(health_center)
             y = ["> %s = %s" % (a,b) for (a,b) in y] if y else ['> None']
             
-            return "Registered patients today:\n%s\nDefaulting patients:\n%s" % ('\n'.join(x), '\n'.join(y))
+            z = self.db_get_defaults(health_center)
+            if z:
+                z = "\nDefaulting patients(ids):\n> %s" % ', '.join(z)
+            else:
+                z = ''
+            
+            return "== NThC report for %s ==\nRegistered patients today:\n%s\nDefaulting patients:\n%s%s" % (health_center if health_center else 'ALL health centers', '\n'.join(x), '\n'.join(y), z)
         elif mode == 'pre':
             x = self.get_pre_apt(health_center)
             x = ["> %s = %s" % (a,b) for (a,b) in x] if x else ['> None']
             
-            return "Patients scheduled for today:\n%s" % ('\n'.join(x), )
+            return "== NThC report for %s ==\nPatients scheduled for today:\n%s" % (health_center if health_center else 'ALL health centers', '\n'.join(x), )
         else:
             return ''
     
