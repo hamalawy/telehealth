14ef07e392d096b8e94050576d500ddf339e07c5
ortegajerome <ortegajerome@34d7ff0e-e4a4-11dd-a6ea-a32d6b26a33d>
2009-08-13 01:04:58 +0000

    ECCS-DEVIEWER Integrated edf parser

--- lifelink/rxbox/trunk/triageviewer/getdata.php
+++ lifelink/rxbox/trunk/triageviewer/getdata.php
@@ -4,7 +4,7 @@ $link = mysql_connect("localhost","root","telemedicine311") or die ("Unable to c
 mysql_select_db("test_edf",$link) or die ("Unable to select database");
 
 header("Content-type: text/plain");
-    $data = mysql_query('SELECT val FROM edfs where id=4');
+    $data = mysql_query('SELECT val FROM edfs ORDER BY id DESC LIMIT 1');
     if (!$data) die;
     $x = mysql_fetch_assoc($data);
     echo $x['val'];
