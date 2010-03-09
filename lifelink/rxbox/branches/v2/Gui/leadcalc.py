f8706908ec204d0b2d0f95095df324e31ba82b5c
thomasrodinel <thomasrodinel@34d7ff0e-e4a4-11dd-a6ea-a32d6b26a33d>
2010-03-09 18:19:17 +0000

    Pydoc for lead calc

--- lifelink/rxbox/branches/v2/Gui/leadcalc.py
+++ lifelink/rxbox/branches/v2/Gui/leadcalc.py
@@ -1,6 +1,27 @@
+"""
+RxBox Lead Calculator Module: contains methods that computes for lead I and
+the three augmented leads
+
+Authors: Dan Simone M. Cornillez
+		 Luis Sison, PhD
+         ------------------------------------------------
+         Instrumentation, Robotics and Control Laboratory
+         University of the Philippines - Diliman
+         ------------------------------------------------
+         October 2009		 
+		 
+"""
 #function to calculate lead I
 def calcLI(leadII,leadIII):
-
+    """
+    function that computes for lead I. Computes the lead I using the following
+    equations: lead I = lead II - lead III
+    Parameters:
+    lead II: list of lead II values
+    lead III: list of lead III values
+    Returns:
+    lead I: list of lead I values
+    """
     leadI=[]
     for i in range(len(leadII)):
     
@@ -10,7 +31,15 @@ def calcLI(leadII,leadIII):
 
 #function to calculate lead VL
 def calcLVL(leadII,leadIII):
-
+    """
+    function that computes for augmented lead VL. Computes for lead aVL using the following
+    equations: lead aVL = 0.5*lead II - lead III
+    Parameters:
+    lead II: list of lead II values
+    lead III: list of lead III values
+    Returns:
+    lead aVL: list of lead aVL values
+    """
     leadaVL = []
     for i in range(len(leadII)):
         leadaVL.append((-leadIII[i])+(0.5*leadII[i]))
@@ -18,7 +47,15 @@ def calcLVL(leadII,leadIII):
 
 #function to calculate lead VF
 def calcLVF(leadII,leadIII):
-
+    """
+    function that computes for augmented lead VF. Computes for lead aVF using the following
+    equations: lead aVF = 0.5*(lead II + lead III)
+    Parameters:
+    lead II: list of lead II values
+    lead III: list of lead III values
+    Returns:
+    lead aVF: list of lead aVF values
+    """
     leadaVF = []
     for i in range(len(leadII)):
         leadaVF.append(0.5*(leadIII[i]+leadII[i]))
@@ -26,7 +63,15 @@ def calcLVF(leadII,leadIII):
     return leadaVF
 #function to calculate lead VR
 def calcLVR(leadII,leadIII):
-
+    """
+    function that computes for augmented lead VR. Computes for lead aVR using the following
+    equations: lead aVR = 0.5*(lead III - lead II)
+    Parameters:
+    lead II: list of lead II values
+    lead III: list of lead III values
+    Returns:
+    lead aVR: list of lead aVR values
+    """
     leadaVR = []
     for i in range(len(leadII)):
         leadaVR.append(0.5*(leadIII[i]-leadII[i]))
