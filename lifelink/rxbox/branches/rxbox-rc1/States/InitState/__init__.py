c0f4dd65eaec7743a414013fdafbf2c0476ceb7e
sylukewicent <sylukewicent@34d7ff0e-e4a4-11dd-a6ea-a32d6b26a33d>
2010-08-31 10:44:12 +0000

    Port Allocation Fix

--- lifelink/rxbox/branches/rxbox-rc1/States/InitState/__init__.py
+++ lifelink/rxbox/branches/rxbox-rc1/States/InitState/__init__.py
@@ -16,23 +16,26 @@ class InitState:
     def start(self):
         print 'State Machine: InitState Start'
 
-        #dynamic port allocation
-        comm = subprocess.Popen("dmesg|grep usb\ 5-1|grep FTDI", shell=True, stdout=subprocess.PIPE)
-        ecgport=comm.stdout.read().split('ttyUSB')[-1].strip()
-        print 'ECG: /dev/ttyUSB%s'%ecgport[0]
-        self._config.set('ECG', 'port', '/dev/ttyUSB%s'%ecgport[0])
-        
-        comm = subprocess.Popen("dmesg|grep usb\ 1-6.4|grep pl2303", shell=True, stdout=subprocess.PIPE)
-        spoport=comm.stdout.read().split('ttyUSB')[-1].strip()
-        print 'SPO2: /dev/ttyUSB%s'%spoport[0]
-        self._config.set('SPO2', 'port', '/dev/ttyUSB%s'%spoport[0])
-        
-        comm = subprocess.Popen("dmesg|grep usb\ 1-6.3|grep ch341-uart", shell=True, stdout=subprocess.PIPE)
-        bpport=comm.stdout.read().split('ttyUSB')[-1].strip()
-        print 'BP: /dev/ttyUSB%s'%bpport[0]
-        self._config.set('BP', 'port', '/dev/ttyUSB%s'%bpport[0])
+        try:
+            #dynamic port allocation
+            comm = subprocess.Popen("dmesg|grep usb\ 5-1|grep FTDI", shell=True, stdout=subprocess.PIPE)
+            ecgport=comm.stdout.read().split('ttyUSB')[-1].strip()
+            print 'ECG: /dev/ttyUSB%s'%ecgport[0]
+            self._config.set('ECG', 'port', '/dev/ttyUSB%s'%ecgport[0])
+            
+            comm = subprocess.Popen("dmesg|grep usb\ 1-6.4|grep pl2303", shell=True, stdout=subprocess.PIPE)
+            spoport=comm.stdout.read().split('ttyUSB')[-1].strip()
+            print 'SPO2: /dev/ttyUSB%s'%spoport[0]
+            self._config.set('SPO2', 'port', '/dev/ttyUSB%s'%spoport[0])
+            
+            comm = subprocess.Popen("dmesg|grep usb\ 1-6.3|grep ch341-uart", shell=True, stdout=subprocess.PIPE)
+            bpport=comm.stdout.read().split('ttyUSB')[-1].strip()
+            print 'BP: /dev/ttyUSB%s'%bpport[0]
+            self._config.set('BP', 'port', '/dev/ttyUSB%s'%bpport[0])
 
-        self._config.write(open('rxbox.cfg', 'w'))
+            self._config.write(open('rxbox.cfg', 'w'))
+        except:
+            pass
 
         self._frame = RxboxFrame(self._engine, None, -1, "")
         self._mgr = self._frame._mgr
