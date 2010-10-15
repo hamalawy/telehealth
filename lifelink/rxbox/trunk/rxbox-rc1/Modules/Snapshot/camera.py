d56ff9f52202969cfb1f76f6d15bd2e991df93d0
sylukewicent <sylukewicent@34d7ff0e-e4a4-11dd-a6ea-a32d6b26a33d>
2010-10-15 09:33:49 +0000

    Snapshot Fix

---
+++ lifelink/rxbox/trunk/rxbox-rc1/Modules/Snapshot/camera.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import ConfigParser
+import os
+from subprocess import *
+import time
+from datetime import datetime
+
+"""
+tnow = datetime.now()
+tnow = tnow.strftime("%Y_%m_%d_%H_%M_%S")+("_%s"%tnow.microsecond.__str__().replace('.',''))
+print tnow
+
+filename = tnow + '.jpg'
+
+pathname = '/home/tim/Desktop/Tim/telehealth/trunk/rxbox-rc1/Modules/Snapshot/webcam.cfg'
+
+config = ConfigParser.ConfigParser()
+config.read(pathname)
+config.set('grab', 'archive', filename)
+config.set('ftp', 'file', filename)
+with open('webcam.cfg', 'wb') as configfile:
+    config.write(configfile)
+"""
+print 'webcam %s/Modules/Snapshot/webcam.cfg'%os.getcwd()
+p = Popen('webcam %s/Modules/Snapshot/webcam.cfg'%os.getcwd(), shell=True)
+time.sleep(2)
+pid = p.pid + 1
+command = 'kill -15 ' + str(pid)
+os.system(command)
+p.kill()
+
+
