511c448ed2678200bb7083fc2ef5d8dc595d6e6d
sylukewicent <sylukewicent@34d7ff0e-e4a4-11dd-a6ea-a32d6b26a33d>
2011-03-12 05:46:08 +0000

    EDF - ECG Filter Data

---
+++ lifelink/rxbox/trunk/rxbox-rc1/Modules/ECG/FIRfilter/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include"BPfilter.h"
+
+using namespace std;
+
+int main (void) {
+    char buffer;
+    while (true) {
+        int number = 0;
+        int neg = 1;
+        while(true) {
+            buffer = getchar();
+            if(buffer == '-') neg = -1;
+            else if(buffer >= '0' && buffer <= '9') number = number*10 + buffer - '0';
+            else if(buffer == ',') break;
+            else if(buffer == '*') goto stop;
+            else continue;
+        }
+        number *= neg;
+        printf("%d,",number);
+//        printf("%d,",oBPfilterFS500(number, 2));
+    }
+    stop:
+    return 0;
+}
